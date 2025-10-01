e4464a8db9bde273748b4484352b5cbf844d3003
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-10-01 15:50:52 +0530

    Updating Link

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -25,7 +25,7 @@ const ContactForm = () => {
         setSubmitStatus('');
 
         try {
-            const response = await fetch("http://localhost:5000/api/contact", {
+            const response = await fetch("https://beryfy.onrender.com/api/contact", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
