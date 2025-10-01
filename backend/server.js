ef8af044a5687fe37881c1fb2893c4b16fb376f1
Beryfy <beryfy2@gmail.com>
2025-10-02 01:17:15 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://beryfy1.netlify.app/",
         ],
         credentials: true,
     })
