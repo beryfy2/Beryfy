dc5bd5507534b1261ca3b3e5aa96c14241080910
Beryfy <beryfy2@gmail.com>
2025-10-02 01:13:03 +0530

    Update

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -25,7 +25,7 @@ const ContactForm = () => {
         setSubmitStatus('');
 
         try {
-            const response = await fetch("https://beryfy.onrender.com/api/contact", {
+            const response = await fetch("https://beryfy1.onrender.com/api/contact", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
