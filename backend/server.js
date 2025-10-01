36a0b9576ab046c8a174721bf25490afbaa38acc
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-10-01 17:05:48 +0530

    Linking

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://beryfy.netlify.app",
+            "https://beryfy2.netlify.app",
         ],
         credentials: true,
     })
